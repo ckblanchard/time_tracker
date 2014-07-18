d5e2e0dbd037d8f3ede9cbf886d1bcff80f53e43
Craig Blanchard <cbdev.io@gmail.com>
2014-07-17 23:04:32 -0700

    add profile styling and validators

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,28 @@
 class ProfileController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :load_profile
 
   def show
-    @profile = current_user.profile
   end
+
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: "Profile saved!"
+    else
+      # redirect_to edit_profile_path, flash: { error: "There was an error. Please try again."}
+      render :edit, alert: "There was an error. Please try again."
+    end
+  end
+
+  private
+
+    def load_profile
+      @profile = current_user.profile
+    end
+
+    def profile_params
+      params.require(:profile).permit(:name, :address, :address2, :city, :state, :zip_code, :phone_number)
+    end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :zip_code, numericality: { only_integer: true }, allow_blank: true
+  validates :phone_number, format: { with: /\A[+]?\d+\Z/, message: "Invalid phone number."}, allow_blank: true
+  validates_with StateValidator, allow_blank: true
+
 end
