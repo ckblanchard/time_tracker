d8fae7f9719d96c0aa6792f19f5f6b79d8b18834
Craig Blanchard <cbdev.io@gmail.com>
2014-07-17 23:16:37 -0700

    fix phone number validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,8 @@ class Profile < ActiveRecord::Base
   belongs_to :user
 
   validates :zip_code, numericality: { only_integer: true }, allow_blank: true
-  validates :phone_number, format: { with: /\A[+]?\d+\Z/, message: "Invalid phone number."}, allow_blank: true
+  # validates :phone_number, format: { with: /\A[+]?\d+[-]\Z/, message: "Please enter a phone number without dashes"}, allow_blank: true
+  validates :phone_number, format: { with: /\A\d{3}\p{Pd}?\d{3}\p{Pd}?\d{4}/, message: "Please enter a valid phone number with or without dashes." }, allow_blank: true
   validates_with StateValidator, allow_blank: true
 
 end
