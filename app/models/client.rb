6182a0a09070f83faba70e70c49e35cb4b34de4e
Craig Blanchard <cbdev.io@gmail.com>
2014-07-28 16:30:22 -0700

    Moved state_validator into app/validators directory

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,9 +5,6 @@ class Client < ActiveRecord::Base
 
   validates :company, :address, :city, presence: true
   validates :zip_code, numericality: { only_integer: true }, length: { is: 5 }
-  # validates :zip_code, format: { with: /\A\d{5}\z/ }
-  # validates :phone_number, format: { with: /\A\(?\d{3}(\)?\p{Pd}?.?)\d{3}\p{Pd}?\d{4}/, message: "Please enter a valid phone number with or without dashes." }, allow_blank: true
-  # validates :phone_number, phone_number: { ten_digits: true, allow_blank: true, message: "Please enter a valid phone number, including area code." }
   validates :phone_number, format: { with: /\A\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})\z/, message: "Please enter a valid phone number with area code."}, allow_blank: true
   validates_with StateValidator
 end
